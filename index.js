5701f456e143e590546a76983b7424693e09a8da
vansh-r <nileshvansh96@gmail.com>
2024-10-11 21:39:48 +0530

    new things

---
+++ index.js
@@ -0,0 +1,13 @@
+//calculator program 
+const display = document.getElementById("display");
+
+
+function appendToDisplay(input){
+        display.value += input;
+}
+function cleardisplay(){
+
+}
+function calculate(){
+
+}
