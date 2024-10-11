10d6383f07c33128dcd2add6daf7a37043f2dd1a
vansh-r <nileshvansh96@gmail.com>
2024-10-11 22:24:02 +0530

    new

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ function appendToDisplay(input){
        
     display.value += input;
 }
+
 function cleardisplay(){
     
     display.value = "";
 }
 function calculate(){
-
+try{
 display.value = eval(display.value);
+}
+catch(error){
+    display.value = "error"
+}
 }
